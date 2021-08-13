8502d7fae5a15e8e3ee9950ede15910499d151b3
Tiffany Deakin <tdeakin93@gmail.com>
2021-08-12 20:26:34 -0500

    Updates

---
+++ static/js/main.64233013.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbucket=this.webpackJsonpbucket||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),s=c(3),i=c.n(s),o=c(0);var u=function(){return Object(o.jsx)("div",{children:"Hello!"})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(u,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.64233013.chunk.js.map
